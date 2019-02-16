b01b3fae9a796d99e0874ef3475ebf926e87ffdd
Jon Sharp Jr <sharp-jr@royal-13.cs.wisc.edu>
2019-02-15 21:10:37 -0600

    almost done

--- hw3_2/rca_16b.v
+++ hw3_2/rca_16b.v
@@ -19,8 +19,8 @@ module rca_16b(A, B, C_in, S, C_out);
 
     // YOUR CODE HERE
 	rca_4b rca_lsb_4(.A(A[3:0]), .B(B[3:0]), .C_in(C_in), .C_out(c_out[0]), .S(S[3:0]));
-	rca_4b rca_lsb_2_4(.A(A[4 + 3 : 4]), .B(B[4 + 3 : 4]), .C_in(c_out[i - 1]), .S(S[4 + 3 : 4]), .C_out(c_out[i]));
-	rca_4b rca_lsb_2_4(.A(A[2*4 + 3 : 2*4]), .B(B[2*4 + 3 : 2*4]), .C_in(c_out[i - 1]), .S(S[2*4 + 3 : 2*4]), .C_out(c_out[i]));
+	rca_4b rca_lsb_1_4(.A(A[4 + 3 : 4]), .B(B[4 + 3 : 4]), .C_in(c_out[0]), .S(S[4 + 3 : 4]), .C_out(c_out[1]));
+	rca_4b rca_lsb_2_4(.A(A[2*4 + 3 : 2*4]), .B(B[2*4 + 3 : 2*4]), .C_in(c_out[1]), .S(S[2*4 + 3 : 2*4]), .C_out(c_out[2]));
 	rca_4b rca_msb_4(.A(A[N - 1: N - 4]), .B(B[N - 1: N - 4]), .C_in(c_out[N_4 - 2]), .C_out(C_out), .S(S[N - 1: N - 4]));
 
 endmodule
